8b64b5d65dd503b8117fbf9f36e5ea44a058a968
DeathWalker47 <fizer_00@mail.ru>
2021-08-08 17:39:37 +0300

    added top section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
-  
+  $('.top-slider').slick({
+    infinite: true,
+    dots: true,
+    speed: 1000,
+    customPaging : function(slider, i) {
+      var thumb = jQuery(slider.$slides[i]).data();
+      return '<a>'+('0'+(i+1)).slice(-2)+'</a>'; 
+  },
+  cssEase: 'linear',
+
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/decor/left-arrow.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/decor/right-arrow.svg" alt=""></button>',
+  });
+  AOS.init();
 });
+
